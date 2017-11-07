06d0e758358331bf5978bfc48ae7c54f61b1871b
albertog@V110-15ISK <albertoguerrerogarcia@gmail.com>
2017-11-07 01:31:12 +0100

    Start Menu and link model

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,13 +8,7 @@
 </div> 
 
 <div class="container">
-    @if (session('status'))
-        <div class="alert alert-success">
-            {{ session('status') }}
-        </div>
-    @endif
-
-    You are logged in!
+    
 </div>
 
 @endsection
